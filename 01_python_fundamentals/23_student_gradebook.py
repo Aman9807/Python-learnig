764fa21c9e00b9de9ccd94d5b07c896659dff5c9
Aman9807 <aman@example.com>
2026-08-09 17:20:24 +0530

    Complete Question 23: Student Gradebook System

--- 01_python_fundamentals/23_student_gradebook.py
+++ 01_python_fundamentals/23_student_gradebook.py
@@ -18,4 +18,65 @@
 # 4. Identify and display the **Class Topper** (the student with the highest average mark).
 #
 # Write your code below this line:
+gradebook = [
+    {
+        "name": "Aman",
+        "marks": {"Maths":00,"Science": 0, "English":0}
+    },          
+    {
+        "name": "Aman",
+        "marks": {"Maths": 00, "Science": 0, "English": 0}
+    },
+    {
+        "name": "Aman",
+        "marks": {"Maths": 00, "Science": 0, "English": 0}
+    }
+]
+
+def get_student_average(student_dict):
+    sum = 0
+    for value in student_dict.values():
+        sum += value
+
+    return sum/len(student_dict)
+
+def get_student_total_marks(student_dict):
+    return sum(student_dict.values())
+
+
+
+for i in range(3):
+    gradebook[i]["name"] = input("Enter student name: ")
+    gradebook[i]["marks"]["Maths"] = int(input("Enter marks in Maths: "))
+    gradebook[i]["marks"]["Science"] = int(input("Enter marks in Science: "))
+    gradebook[i]["marks"]["English"] = int(input("Enter marks in English: "))
+    
+hishest_average = 0
+topper_name = ""
+total_marks = 0
+average = 0
+grade = ""
+
+highest_average = 0
+topper_name = ""
+
+# Loop through the list of student dictionaries
+for student in gradebook:
+    average = get_student_average(student['marks'])
+    total_marks = get_student_total_marks(student['marks'])
+    
+    if highest_average < average:
+        highest_average = average
+        topper_name = student['name']
+
+    if average >= 40:
+        grade = "PASSED"
+
+    else:
+        grade = "FAILED"
+        
+    print(f"Student: {student['name']} | Total Marks: {total_marks} | Average Score: {average:.2f} | Status: {grade}")
+
+print(f"\nTopper is {topper_name} with highest average score of {highest_average:.2f}")
+
 
